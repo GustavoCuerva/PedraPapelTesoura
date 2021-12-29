b410ba6d2aaf57c1bd16faf9930984d70dbfe0f4
Gustavo Cuerva <cuervagustavo@gmail.com>
2021-12-29 11:37:29 -0300

    Adicionando jogada do computador

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 var elementos = document.querySelectorAll('.player div');
+var IA = document.querySelectorAll('.IA div');
 var playerOPT = "";
+var iaOPT = "";
 
 //Baixar opacidade
-function clearOpacity(){
+function clearOpacityIA(){
+    IA.forEach((x,i)=>{
+        IA[i].style.opacity = 0.3;
+    });
+}
+
+function clearOpacityPlayer(){
     elementos.forEach((x,i)=>{
         elementos[i].style.opacity = 0.3;
     });
 }
 
+//Seleção do Computador
+function jogarIA(){
+    let mao = Math.floor(Math.random()*3);
+
+    IA.forEach((x,i) => {
+        if(i == mao){
+            clearOpacityIA();
+            x.style.opacity = 1;
+            iaOPT = x.childNodes[0].getAttribute('opt');
+        }
+    });
+    alert(playerOPT);
+    alert(iaOPT);
+}
+
 //Seleção do Jogador
 elementos.forEach((x,i) => {
     x.addEventListener('click', (t)=>{
-        clearOpacity();
+        clearOpacityPlayer();
         x.style.opacity = 1;
         playerOPT = t.target.getAttribute('opt');
-        alert(playerOPT);
+        jogarIA();
     });
 });
