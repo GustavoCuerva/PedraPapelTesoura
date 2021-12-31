6c46449e12641521d5a461c0679fa7ff847328b1
Gustavo Cuerva <cuervagustavo@gmail.com>
2021-12-31 15:35:50 -0300

    Opção de reiniciar e começar novamente adicionadas

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var elementos = document.querySelectorAll('.player div');
 var IA = document.querySelectorAll('.IA div');
 var container = document.querySelector('.aling');
 var opcoes = document.querySelector('.config');
-var reiniciar = document.querySelector('.reiniciar');
-var sair = document.querySelector('.sair');
+var btns = document.querySelector('.btns');
 var rodadasRestantes = document.querySelector('.rodadasRestante span');
 var span_placarPlayer = document.querySelector('.placarJogador');
-var spna_placarIA = document.querySelector('.placarIA');
+var span_placarIA = document.querySelector('.placarIA');
+var popVencedor = document.querySelector('.vencedor');
 var placarPlayer = 0;
 var placarIA = 0;
 var numRodadas;
@@ -30,8 +30,7 @@ jogar.addEventListener('click', ()=>{
     }else{
     if (container.classList.contains('none')) {
         
-        reiniciar.classList.remove('none');
-        sair.classList.remove('none');
+        btns.classList.remove('none');
         container.classList.remove('none');
         opcoes.classList.add('none');
 
@@ -141,13 +140,16 @@ function vencedor(){
 function menosRodada(){
     numRodadas--;
     rodadasRestantes.innerHTML = numRodadas;
+    if (numRodadas==0) {
+        endGame();
+    }
 }
 
 //Derrota
 
 function derrota(){
     placarIA++;
-    spna_placarIA.innerHTML = placarIA;
+    span_placarIA.innerHTML = placarIA;
 }
 
 //Vitória
@@ -155,4 +157,72 @@ function derrota(){
 function vitoria(){
     placarPlayer++;
     span_placarPlayer.innerHTML = placarPlayer;
+}
+
+//Fim de jogo
+
+function endGame(){
+    if (popVencedor.classList.contains('none')) {
+        
+        btns.classList.add('none');
+        container.classList.add('none');
+        popVencedor.classList.remove('none');
+
+        document.querySelector('.placarJ').innerHTML = placarPlayer;
+        document.querySelector('.placarI').innerHTML = placarIA;
+
+        let msg = document.querySelector('.vencedor h2');
+
+        if (placarPlayer>placarIA) {
+            msg.innerHTML = 'Parabéns';
+        }else if(placarIA>placarPlayer){
+            msg.innerHTML = 'Foi por pouco, tente novamente';
+        }else if(placarPlayer==placarIA){
+            msg.innerHTML = 'Passou perto, bom empate';
+        }
+    }
+}
+
+//Jogar novamente com configurações diferentes
+function jogarNovamente(){
+    if (popVencedor.classList.contains('none')) {
+    }else{
+        popVencedor.classList.add('none');
+    }
+    if (container.classList.contains('none')) {
+    }else{
+        btns.classList.add('none');
+        container.classList.add('none');
+    }
+
+    document.querySelector('input[name=rodadas]').value = '';
+    placarPlayer = 0;
+    span_placarPlayer.innerHTML = placarPlayer;
+
+    placarIA = 0;
+    span_placarIA.innerHTML = placarIA;
+
+    opcoes.classList.remove('none');
+}
+
+
+//Reiniciar o Jogo com as mesmas configurações
+function reiniciar(){
+    
+    if (popVencedor.classList.contains('none')) {
+    }else{
+        popVencedor.classList.add('none');
+        btns.classList.remove('none');
+        container.classList.remove('none');
+    }
+
+    
+    numRodadas = parseInt(quantRodadas);
+    rodadasRestantes.innerHTML = numRodadas;
+
+    placarPlayer = 0;
+    span_placarPlayer.innerHTML = placarPlayer;
+
+    placarIA = 0;
+    span_placarIA.innerHTML = placarIA;
 }
